105ded3e0bfba957373ab2960304483dbfdb7591
w-beom <w_bum@naver.com>
2023-01-18 01:21:22 +0900

    feat : index 화면 생성
    - index 화면 생성
    - Controller 생성

--- src/main/java/com/woo/shorturl/ShortUrlApplication.java
+++ src/main/java/com/woo/shorturl/ShortUrlApplication.java
@@ -1,4 +1,4 @@
-package com.example.shorturl;
+package com.woo.shorturl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/woo/shorturl/ShortUrlApplicationTests.java
+++ src/test/java/com/woo/shorturl/ShortUrlApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.shorturl;
+package com.woo.shorturl;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
