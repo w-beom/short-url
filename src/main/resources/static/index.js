6a7d057500d699b0e366f98bba4891493cb43d1d
w-beom <w_bum@naver.com>
2023-01-26 02:34:46 +0900

    feat : axios 추가, 서버와 통신기능 추가
    - 사용자가 url 입력 후 서버와 통신기능 추가
    - 서버 통신 후 단축 URL을 텍스트로 그려줌

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,5 +1,24 @@
 const submitButton = document.getElementById("submit-btn");
+const input = document.getElementById("input-url");
+const shortUrlText = document.getElementById("short-url-text")
 
 submitButton.addEventListener('click', () => {
-
+    axios({
+        method: 'post',
+        url: '/short-url',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: {
+            url: input.value
+        }
+    })
+        .then(function (response) {
+            let shortUrl = response.data.shortUrl;
+            let text = document.createTextNode(shortUrl.shortUrl);
+            shortUrlText.appendChild(text);
+        })
+        .catch(function (error) {
+            alert(error.message);
+        });
 });
