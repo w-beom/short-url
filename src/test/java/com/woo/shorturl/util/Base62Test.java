529de9a9a460906f058e3bfd31b99fa1652816bd
w-beom <w_bum@naver.com>
2023-01-27 20:28:30 +0900

    test : Base62 테스트코드 변경

--- src/test/java/com/woo/shorturl/util/Base62Test.java
+++ src/test/java/com/woo/shorturl/util/Base62Test.java
@@ -10,10 +10,10 @@ class Base62Test {
     @Test
     @DisplayName("BASE62 인코딩 테스트")
     public void test01() {
-        String str = "http://www.naver.com/sdoajspsaojpsojdjopdspojpadojaspojdpsd";
-        String encode = Base62.encode(str);
-        System.out.println(encode);
-        assertEquals(Base62.encode(str), Base62.encode(str));
+        long id = 123123123L;
+        String encode = Base62.encode(id);
+        long decode = Base62.decode(encode);
+        assertEquals(id, decode);
     }
 
 }
