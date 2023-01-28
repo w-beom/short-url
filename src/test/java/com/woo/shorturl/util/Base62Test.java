fc0ca17cfc36e298cea5efaf9f5aef38b5d38afa
w-beom <w_bum@naver.com>
2023-01-28 22:23:37 +0900

    test : Base62 테스트 코드 변경

--- src/test/java/com/woo/shorturl/util/Base62Test.java
+++ src/test/java/com/woo/shorturl/util/Base62Test.java
@@ -8,11 +8,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class Base62Test {
 
     @Test
-    @DisplayName("BASE62 인코딩 테스트")
+    @DisplayName("id를 Base62로 인코딩 후 디코딩한 결과 값이 같다.")
     public void test01() {
+        // given
         long id = 123123123L;
+
+        // when
         String encode = Base62.encode(id);
         long decode = Base62.decode(encode);
+
+        //then
         assertEquals(id, decode);
     }
 
