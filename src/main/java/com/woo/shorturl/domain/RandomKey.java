389ef78c09aae0b3f081d4d7c9ad18ac22622b9f
w-beom <w_bum@naver.com>
2023-02-02 22:04:40 +0900

    feat : 랜덤키를 만드는 클래스 생성

---
+++ src/main/java/com/woo/shorturl/domain/RandomKey.java
@@ -0,0 +1,9 @@
+package com.woo.shorturl.domain;
+
+public class RandomKey implements KeyGenerator {
+    @Override
+    public String create() {
+        // doSomething
+        return "";
+    }
+}
